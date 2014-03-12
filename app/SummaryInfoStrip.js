93d17d92207bdf2c80822a45538750d4ab1d178f
Lee Bock <lbock@esri.com>
2014-03-12 15:31:30 -0400

    more styling adjustments

--- app/SummaryInfoStrip.js
+++ app/SummaryInfoStrip.js
@@ -2,25 +2,31 @@ function SummaryInfoStrip(div)
 {
 	var _div = div;
 	
-	var _title = $("<div class='title'>Total Tornadoes</div>");
+	var _year = $("<div class='infoValue' style='font-size:36px'></div>");
 	var _total = $("<div class='superBigGreenText'></div>");
-	var _captionYear = $("<div class='infoCaption'></div>");
-	
 	var _injuries = $("<div class='infoValue'></div>");
+	var _fatalities = $("<div class='infoValue'></div>");
 	
-	$(_div).append(_title);
+	$(_div).append(_year);
+	$(_div).append("<div class='dateCaption'>Year</div>");
+	
 	$(_div).append(_total);
 	$(_div).append("<hr/>");
-	$(_div).append(_captionYear);
+	$(_div).append("<div class='infoCaption'>Total tornadoes</div>");
 	
 	$(_div).append(_injuries);
 	$(_div).append("<hr/>");
-	$(_div).append("Injuries");
+	$(_div).append("<div class='infoCaption'>Injuries</div>");
 	
-	this.updateInfo = function(year, total, injuries)
+	$(_div).append(_fatalities);
+	$(_div).append("<hr/>");
+	$(_div).append("<div class='infoCaption'>Fatalities</div>");
+		
+	this.updateInfo = function(year, total, injuries, fatalities)
 	{
+		$(_year).html(year);	
 		$(_total).html(total);	
-		$(_captionYear).html(year);	
 		$(_injuries).html(injuries);
+		$(_fatalities).html(fatalities);
 	}
 }
