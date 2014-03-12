76f117fccdaaaf443bcebd44465f4a017e561cf9
Lee Bock <lbock@esri.com>
2014-03-12 14:22:17 -0400

    further building out SummaryInfoStrip to include injuries.

--- app/SummaryInfoStrip.js
+++ app/SummaryInfoStrip.js
@@ -6,14 +6,21 @@ function SummaryInfoStrip(div)
 	var _total = $("<div class='superBigGreenText'></div>");
 	var _captionYear = $("<div class='infoCaption'></div>");
 	
+	var _injuries = $("<div class='infoValue'></div>");
+	
 	$(_div).append(_title);
 	$(_div).append(_total);
 	$(_div).append("<hr/>");
 	$(_div).append(_captionYear);
 	
-	this.updateInfo = function(year, total)
+	$(_div).append(_injuries);
+	$(_div).append("<hr/>");
+	$(_div).append("Injuries");
+	
+	this.updateInfo = function(year, total, injuries)
 	{
 		$(_total).html(total);	
 		$(_captionYear).html(year);	
+		$(_injuries).html(injuries);
 	}
 }
