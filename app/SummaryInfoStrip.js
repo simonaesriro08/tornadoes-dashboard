12e05cd04b1b638b924a5fa1295f49c73e359b6c
Lee Bock <lbock@esri.com>
2014-03-12 11:40:05 -0400

    SummaryInfoStrip: begin to build out actual fields

--- app/SummaryInfoStrip.js
+++ app/SummaryInfoStrip.js
@@ -1,9 +1,12 @@
 function SummaryInfoStrip(div)
 {
 	var _div = div;
+	
+	var _total = $("<div class='infoValue'></div>");
+	$(_div).append(_total);
+	
 	this.updateInfo = function(year, total)
 	{
-		var text = "In <b>"+year+"</b>, the area to the right saw <b>"+total+"</b> tornadoes.";
-		$(_div).html(text);		
+		$(_total).html(total);		
 	}
 }
