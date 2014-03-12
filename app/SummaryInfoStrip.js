c2c9beb07318f3c34bff9201da41774bd6f9de33
Lee Bock <lbock@esri.com>
2014-03-12 11:58:45 -0400

    building out SummaryInfoStrip

--- app/SummaryInfoStrip.js
+++ app/SummaryInfoStrip.js
@@ -2,11 +2,18 @@ function SummaryInfoStrip(div)
 {
 	var _div = div;
 	
-	var _total = $("<div class='infoValue'></div>");
+	var _title = $("<div class='title'>Total Tornadoes</div>");
+	var _total = $("<div class='superBigGreenText'></div>");
+	var _captionYear = $("<div class='infoCaption'></div>");
+	
+	$(_div).append(_title);
 	$(_div).append(_total);
+	$(_div).append("<hr/>");
+	$(_div).append(_captionYear);
 	
 	this.updateInfo = function(year, total)
 	{
-		$(_total).html(total);		
+		$(_total).html(total);	
+		$(_captionYear).html(year);	
 	}
 }
