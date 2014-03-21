248b276cef85e03417fba47ca3c3aa34737ff3eb
Lee Bock <lbock@esri.com>
2014-03-21 15:30:15 -0400

    for some reason, the Fujita info icon qtip wasn't working after one slideOut and retract.  So, I just reset it on every slideOut.

--- app/main.js
+++ app/main.js
@@ -185,12 +185,7 @@ function finishInit() {
 
     var geocoder = new esri.dijit.Geocoder({map: _map}, "search");
     geocoder.startup();
-	
-	$(".infoIcon").qtip({
-		content:{text:'The Enhanced Fujita Scale measures the relative severity of tornadoes, ranging from EF0, causing light damage, to EF5, leveling buildings and wreaking "inconceivable" damage.'}, 
-		style:{classes: 'qtip-light qtip-rounded qtip-shadow'}
-					});
-	
+		
 }
 
 function onKeyDown(e)
@@ -265,6 +260,10 @@ function presentAtts(atts)
 function slideOut()
 {
 	$("#tornado-info-strip").animate({left: 20});
+	$(".infoIcon").qtip({
+		content:{text:'The Enhanced Fujita Scale measures the relative severity of tornadoes, ranging from EF0, causing light damage, to EF5, leveling buildings and wreaking "inconceivable" damage.'}, 
+		style:{classes: 'qtip-light qtip-rounded qtip-shadow'}
+	});		
 }
 
 function retract()
