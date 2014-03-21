7cc64b05ecf90e0284e04091fa6a28c424b4c786
Lee Bock <lbock@esri.com>
2014-03-21 15:51:43 -0400

    in GISService.summarizeForExtent: 1) add summary statistic for Loss.  2) select for F_Scale > -9. 3) use incoming extent argument (instead of referencing _map.extent directly

--- app/GISService.js
+++ app/GISService.js
@@ -27,16 +27,22 @@ function GISService()
 		statDefFatalities.statisticType = "sum";
 		statDefFatalities.onStatisticField = "Fatalities";
 		statDefFatalities.outStatisticFieldName = "totalFatalities";
+		
+		var statDefLoss = new esri.tasks.StatisticDefinition();
+		statDefLoss.statisticType = "sum";
+		statDefLoss.onStatisticField = "Loss";
+		statDefLoss.outStatisticFieldName = "totalPropertyLoss";
 	
-		var extent = esri.geometry.webMercatorToGeographic(_map.extent)
+		var extent = esri.geometry.webMercatorToGeographic(extent)
 		
 		var query = new esri.tasks.Query();
 		query.where = "Starting_Lat >= "+extent.ymin+
 					" AND Starting_Long >= "+extent.xmin+
 					" AND Starting_Lat <= "+extent.ymax+
 					" AND Starting_Long <= "+extent.xmax+
+					" AND F_Scale > -9"+
 					" AND Year in ('1980','1981','1982','1983','1984','1985','1986','1987','1988','1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012')";
-		query.outStatistics = [statDefCount, statDefInjuries, statDefFatalities];
+		query.outStatistics = [statDefCount, statDefInjuries, statDefFatalities, statDefLoss];
 		query.groupByFieldsForStatistics = ["Year"];
 						
 		var queryTask = new esri.tasks.QueryTask(FEATURE_SERVICE_URL);
